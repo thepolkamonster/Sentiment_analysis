b676161aa9c2c104d27747a03160b8547f52cd0a
Saksham Parashar <72528293+thepolkamonster@users.noreply.github.com>
2023-06-08 17:05:50 +0530

    Update settings.py
    Set DEBUG = True in settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 SECRET_KEY = 'jfeivr6wgxuw__9bxx^jxsv@u&mxdss+j$y-9ityd8&a#q6hrx'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['sentiment-logistic-heroku.herokuapp.com', '127.0.0.1']
 
